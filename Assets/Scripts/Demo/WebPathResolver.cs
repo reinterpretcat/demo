997cc5d04fd60e1be9ca926c19787085e4d51a35
Ilya Builuk <ilya.builuk@gmail.com>
2015-01-24 22:50:49 +0100

    Apply v0.3 version changes

--- Assets/Scripts/Demo/WebPathResolver.cs
+++ Assets/Scripts/Demo/WebPathResolver.cs
@@ -8,10 +8,16 @@ namespace Assets.Scripts.Demo
     {
         public string Resolve(string path)
         {
+            var lowerCase = path.ToLower();
+            if (lowerCase.StartsWith("config") || lowerCase.StartsWith("maps"))
+                path = "Assets//Resources//" + path;
+
             // WEB
-            if (path.EndsWith(".hgt") || path.EndsWith(".pbf") || path.EndsWith(".mapcss"))
-                 return path.Replace(@"\", @"/");
-             return Path.Combine(Path.GetDirectoryName(path), Path.GetFileNameWithoutExtension(path)).Replace(@"\",@"/");
+            if (lowerCase.EndsWith(".mapcss"))
+                return path+ ".txt";
+
+            return path.Replace(@"\", @"/");
+            //return Path.Combine(Path.GetDirectoryName(path), Path.GetFileNameWithoutExtension(path)).Replace(@"\",@"/");
         }
     }
 }
