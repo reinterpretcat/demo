451339196fe45c7d0320f0fa7ceab9503c3ebacf
Ilya Builuk <ilya.builuk@gmail.com>
2015-05-01 00:32:38 +0200

    Add menu scene and the corresponding assets

---
+++ Assets/Scenes/MenuAssets/Scripts/OfflineMapsPanelManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class OfflineMapsPanelManager: PanelManager
+    {
+        void Start()
+        {
+            InitializeInstalledMapData();
+        }
+
+        #region Map import logic
+
+        public InputField PathField;
+        public Button ImportButton;
+
+        public void OnImportClick()
+        {
+            var path = PathField.text;
+            Debug.Log("Import from:" + path);
+        }
+
+        #endregion
+
+        #region Map list logic
+
+        public Text InstalledMapText;
+
+        private void InitializeInstalledMapData()
+        {
+            
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scenes/MenuAssets/Scripts/PlayGamePanelManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class PlayGamePanelManager: PanelManager
+    {
+        public InputField NameInputField;
+        public InputField CoordinateInputField;
+        public Button SearchButton;
+
+        private string[] _results = new string[2]{"Berlin Hauptbahnhof", "Berlin Naturkundemuseum"};
+        private int _currentIndex = 0;
+        private bool _isSearchClick = true;
+
+        private void Start()
+        {
+            NameInputField.onEndEdit.AddListener((_) =>
+            {
+                SearchButton.GetComponentInChildren<Text>().text = "Search";
+                _isSearchClick = true;
+            });
+        }
+
+        public void OnSearch()
+        {
+            if (_isSearchClick)
+            {
+                _currentIndex = 0;
+                _isSearchClick = false;
+                Debug.Log("Launch search for:" + NameInputField.text);
+                // NOTE should be done from different thread
+                ShowResult();
+            }
+            else
+            {
+                _currentIndex++;
+                ShowResult();
+            }
+        }
+
+        private void ShowResult()
+        {
+            if (_results == null || _results.Length == 0)
+                return;
+
+            if (_currentIndex == _results.Length)
+                _currentIndex = 0;
+
+            SearchButton.GetComponentInChildren<Text>().text =
+                String.Format("Result {0} of {1}", _currentIndex + 1, _results.Length);
+
+            NameInputField.text = _results[_currentIndex];
+            CoordinateInputField.text = _results[_currentIndex];
+        }
+    }
+}
