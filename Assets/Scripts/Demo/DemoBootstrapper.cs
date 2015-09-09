b85361c93c0ca2e9915aa2b56ad15c0fbe92bc6e
Ilya Builuk <ilya.builuk@gmail.com>
2015-09-09 23:28:47 +0200

    Commit intermediate progress
    Support multiply model builders

--- Assets/Scripts/Demo/DemoBootstrapper.cs
+++ Assets/Scripts/Demo/DemoBootstrapper.cs
@@ -1,6 +1,5 @@
 ﻿using ActionStreetMap.Core;
-using ActionStreetMap.Explorer.Infrastructure;
-using ActionStreetMap.Infrastructure.Bootstrap;
+using ActionStreetMap.Explorer.Bootstrappers;
 using ActionStreetMap.Infrastructure.Dependencies;
 using ActionStreetMap.Infrastructure.Diagnostic;
 using Assets.Scripts.MapEditor.Behaviors;
@@ -29,9 +28,9 @@ namespace Assets.Scripts.Demo
             // this class will listen messages about tile processing from ASM engine
             _messageListener = new DemoTileListener(_messageBus, _trace);
 
-            // behaviours
-            Container.RegisterInstance(new BehaviourProvider()
-                .Register("terrain_draw", typeof(TerrainDrawBehaviour)));
+            // extensions
+           ExtensionProvider
+               .RegisterBehaviour("terrain_draw", typeof(TerrainDrawBehaviour));
 
             return true;
         }
